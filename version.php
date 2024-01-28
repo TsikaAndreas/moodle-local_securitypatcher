cb570431b57a95c9a8c69f1fb8179aa6abfdd5ef
Andreas Tsika <andreastsika@gmail.com>
2024-01-28 14:29:16 +0200

    Update plugin version

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_securitypatcher';
-$plugin->release = '1.0.0';
-$plugin->version = 2023083008;
+$plugin->release = '1.1.0';
+$plugin->version = 2023083009;
 $plugin->requires = 2022112800;
-$plugin->maturity = MATURITY_ALPHA;
+$plugin->maturity = MATURITY_STABLE;
